6963f75d146857015c3228ea14f1d7961dde552b
Nilson Parra <n.parram@uniandes.edu.co>
2023-11-01 18:32:51 -0500

    feature: view artist list #16

---
+++ app/src/main/java/co/edu/uniandes/miswmobile/vinilosapp/ui/ArtistFragment.kt
@@ -0,0 +1,74 @@
+package co.edu.uniandes.miswmobile.vinilosapp.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.edu.uniandes.miswmobile.vinilosapp.viewmodels.ArtistViewModel
+import co.edu.uniandes.miswmobile.vinilosapp.R
+import co.edu.uniandes.miswmobile.vinilosapp.databinding.ArtistFragmentBinding
+import co.edu.uniandes.miswmobile.vinilosapp.viewmodels.AlbumViewModel
+
+class ArtistFragment : Fragment() {
+
+    private var _binding: ArtistFragmentBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewModel: ArtistViewModel
+    //todo private var viewModelAdapter: ArtistAdater? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = ArtistFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        // todo viewModelAdapter = ArtistAdapter()
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.artistRv
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        //todo recyclerView.adapter = viewModelAdapter
+
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onActivityCreated()"
+        }
+
+        activity.actionBar?.title = getString(R.string.artists)
+        viewModel = ViewModelProvider(this, ArtistViewModel.Factory(activity.application)).get(
+            ArtistViewModel::class.java)
+        //todo
+        /*
+        viewModel.albums.observe(viewLifecycleOwner, Observer<List<Album>> {
+            it.apply {
+                viewModelAdapter!!.albums = this
+            }
+        })
+        */
+        //todo
+        viewModel.eventNetworkError.observe(viewLifecycleOwner, Observer<Boolean> { isNetworkError ->
+            if (isNetworkError) onNetworkError()
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun onNetworkError() {
+        if(!viewModel.isNetworkErrorShown.value!!) {
+            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+            viewModel.onNetworkErrorShown()
+        }
+    }
+
+}
