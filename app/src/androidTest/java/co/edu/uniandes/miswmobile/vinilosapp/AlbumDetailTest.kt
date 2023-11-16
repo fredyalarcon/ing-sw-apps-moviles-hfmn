6461a05d95b9fe460e144216d2e0fe8df5cec4c7
Nilson Parra <n.parram@uniandes.edu.co>
2023-11-15 21:49:28 -0500

    feature: test espresso para detalle de album #15

--- app/src/androidTest/java/co/edu/uniandes/miswmobile/vinilosapp/AlbumDetailTest.kt
+++ app/src/androidTest/java/co/edu/uniandes/miswmobile/vinilosapp/AlbumDetailTest.kt
@@ -1,7 +1,10 @@
 package co.edu.uniandes.miswmobile.vinilosapp
 
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -21,7 +24,7 @@ class AlbumDetailTest {
     )
 
     @Test
-    fun mainActivityAlbumDetailTest(){
+    fun mainActivityAlbumDetailTest() {
         val buttonVisitor = Espresso.onView(
             allOf(
                 ViewMatchers.withId(R.id.buttonVisitor),
@@ -48,11 +51,46 @@ class AlbumDetailTest {
             )
         )
 
-        Espresso.onView(
+        val rvAlbumes = Espresso.onView(
             allOf(
                 ViewMatchers.withId(R.id.albumsRv),
                 ViewMatchers.isDisplayed()
             )
         )
+
+        rvAlbumes.perform(
+            RecyclerViewActions.actionOnItemAtPosition<ViewHolder>(
+                0,
+                ViewActions.click()
+            )
+        )
+        // campo album cover - imagen caratula miniatura
+        Espresso.onView(ViewMatchers.withId(R.id.imageView))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        // campo album cover - imagen caratula
+        Espresso.onView(ViewMatchers.withId(R.id.imageView2))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        // campo album name
+        Espresso.onView(ViewMatchers.withId(R.id.textView6))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        // campo album genero
+        Espresso.onView(ViewMatchers.withId(R.id.textView5))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        // campo album fecha lanzamiento
+        Espresso.onView(ViewMatchers.withId(R.id.textView7))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        // campo album sello discografico
+        Espresso.onView(ViewMatchers.withId(R.id.textView12))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        // campo album descripcion
+        Espresso.onView(ViewMatchers.withId(R.id.textView9))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        // boton canciones
+        Espresso.onView(ViewMatchers.withId(R.id.buttonSongs))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        // boton comentarios
+        Espresso.onView(ViewMatchers.withId(R.id.buttonComments))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
