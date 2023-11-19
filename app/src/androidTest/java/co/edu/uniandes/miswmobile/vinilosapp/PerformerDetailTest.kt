32989380dfb7963522ea316eba67b4dfc65b89d7
nparram <n.parram@uniandes.edu.co>
2023-11-19 12:17:08 -0500

    Feature/19 hu004 pruebas automatizadas detalle artista (#86)
    * feature: pruebas detalle artista #19
    
    * feature: test con espresso para ver detalle de artista #19
    
    * feature: puebas detalle album #19

---
+++ app/src/androidTest/java/co/edu/uniandes/miswmobile/vinilosapp/PerformerDetailTest.kt
@@ -0,0 +1,59 @@
+package co.edu.uniandes.miswmobile.vinilosapp
+
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import co.edu.uniandes.miswmobile.vinilosapp.ui.AccessActivity
+import org.hamcrest.Matchers
+import org.hamcrest.core.AllOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class PerformerDetailTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityScenarioRule(
+        AccessActivity::class.java
+    )
+
+    @Test
+    fun mainActivityPerformerDetailTest() {
+        val buttonVisitor = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.buttonVisitor),
+                ViewMatchers.isDisplayed()
+            )
+        )
+        buttonVisitor.perform(ViewActions.click())
+
+        val buttonArtists = Espresso.onView(
+            AllOf.allOf(
+                ViewMatchers.withId(R.id.buttonArtists),
+                ViewMatchers.isDisplayed()
+            )
+        )
+        buttonArtists.perform(ViewActions.click())
+
+        Espresso.onView(
+            AllOf.allOf(
+                ViewMatchers.withId(R.id.action_bar),
+                ViewMatchers.withText("Artistas"),
+                ViewMatchers.isDisplayed()
+            )
+        )
+
+        val rvArtists = Espresso.onView(
+            AllOf.allOf(
+                ViewMatchers.withId(R.id.artistsRv),
+                ViewMatchers.isCompletelyDisplayed()
+            )
+        )
+
+    }
+}
